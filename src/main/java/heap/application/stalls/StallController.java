2bff90a6fdb0802ca54940f18b3bff1e2a2355c7
God3Tier <76461231+God3Tier@users.noreply.github.com>
2025-06-05 17:08:14 +0800

    fixed packaging and added some necessary dependency

--- src/main/java/heap/application/stalls/StallController.java
+++ src/main/java/heap/application/stalls/StallController.java
@@ -1,10 +1,39 @@
 package heap.application.stalls;
 
-import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
 public class StallController {
     
+    private StallRepo repo;
+    
+    
+    public StallController(StallRepo repo) {
+        this.repo = repo;
+    }
+    
     // how to handle input from frontend using springboot? 
+    // Holy this can be cut down
+    // When we redirect, is it expected for 
+    @GetMapping("/{mealId}{location}{budget}{rating}")
+    public List<Stall> getValidRestaurants(@PathVariable int mealId, @PathVariable String location, @PathVariable int budget, @PathVariable int rating) {
+        List<Stall> mealList = repo.findByMeal_Id(mealId);
+        List<Stall> locationList = repo.findByLocation(location);
+        List<Stall> budgetList = repo.findByMeal_Id(budget);
+        List<Stall> ratingList = repo.findByMeal_Id(rating);
+        
+        return mealList.stream()
+                       .filter(a -> locationList.contains(a) && budgetList.contains(a) && ratingList.contains(a))
+                       .collect(Collectors.toCollection(ArrayList::new));
+    }
+    
     
 }
