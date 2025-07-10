6d0b784ef00ea5dd2e1798eaed25d9e322d9196a
God3Tier <76461231+God3Tier@users.noreply.github.com>
2025-07-10 15:21:01 +0800

    attempted to fix review problem

--- src/main/java/heap/application/review/Review.java
+++ src/main/java/heap/application/review/Review.java
@@ -9,6 +9,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import lombok.Data;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import heap.application.stalls.Stall;
 import heap.application.user.User;
 
@@ -24,10 +26,12 @@ public class Review {
 
     @ManyToOne
     @JoinColumn(name = "stall_id")
+    @JsonIgnore
     private Stall stall;
 
     @ManyToOne
     @JoinColumn(name = "app_user")
+    @JsonIgnore
     private User user;
 
     private String reviewDescription;
