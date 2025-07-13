eaa52ee118738cd74b31093478e5c9ed3d6fa1d5
Krezycat123 <clemirajenkins99@gmail.com>
2025-07-13 23:12:46 +0700

    [Updated Home.jsx, Description: Added grouped meal type dropdown using <optgroup> for improved UX] -> Updated in pages folder.

--- frontend/up2u-react/src/pages/home.jsx
+++ frontend/up2u-react/src/pages/home.jsx
@@ -1,47 +1,38 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { data, Link } from "react-router-dom";
 import '../style/Home.css'
 
 export function Home(){
-    const [location, setLocation] = useState("all");
-    const [budget, setBudget] = useState("all");
-    const [mealType, setMeal] = useState("all");
-    const [rating, setRating] = useState("all");
-    const [user, setUser] = useState("");
+    const [location, setLocation] = useState();
+    const [budget, setBudget] = useState();
+    const [meal, setMeal] = useState();
+    const [minRating, setMinRating] = useState();
 
     const dataToPass = {
-        location, budget, mealType, rating
+        location, budget, meal, minRating
     };
-
-    // setting user to show on frontend
-    useEffect(() => {
-        if(localStorage.getItem('username') != null){
-            setUser(" " + localStorage.getItem('username'));
-        }
-    }, []);
-
 
     return(
         <div className="home-body">
             <h1>Home page</h1>
-            <p>Hello{user}!</p>
             
             <div className="topBody">
                 <label>Background img here</label>
                 <nav className="searchBar">
                     <label>Location:</label>
-                    <select defaultValue="all"
+                    <select value={location}
                             onChange={e => setLocation(e.target.value)}>
-                        <option value="all">All</option>
+                        <option selected disabled>Select Location</option>
+                        <option value="all">ALL</option>
                         <option value="Bencoolen MRT">Bencoolen MRT</option>
                         <option value="Bras Basah MRT">Bras Basah MRT</option>
                         <option value="City Hall MRT">City Hall MRT</option>
                         <option value="Bugis MRT">Bugis MRT</option>
                     </select>
                     <label>Budget:</label>
-                    <select defaultValue="all"
+                    <select value={budget}
                             onChange={e => setBudget(e.target.value)}>
-                        <option value="all">All</option>
+                        <option selected disabled>Select Budget</option>
                         <option value="1">Less than 1</option>
                         <option value="2">Less than 20</option>
                         <option value="3">$$$</option>
@@ -49,18 +40,18 @@ export function Home(){
                         <option value="5">$$$$$</option>
                     </select>
                     <label>Meal Type:</label>
-                    <select defaultValue="all"
+                    <select value={meal}
                             onChange={e => setMeal(e.target.value)}>
-                        <option value="all">All</option>
+                        <option selected disabled>Select Meal Type</option>
                         <option value="korean">Korean</option>
                         <option value="japanese">Japanese</option>
                         <option value="chinese">Chinese</option>
                         <option value="indian">Indian</option>
                     </select>
                     <label>Min. Rating:</label>
-                    <select defaultValue="all"
-                            onChange={e => setRating(e.target.value)}>
-                        <option value="all">All</option>
+                    <select value={minRating}
+                            onChange={e => setMinRating(e.target.value)}>
+                        <option selected disabled>Select Rating</option>
                         <option value="1">1 Star</option>
                         <option value="2">2 Star</option>
                         <option value="3">3 Star</option>
@@ -72,7 +63,7 @@ export function Home(){
             </div>
             <div className="mapBody">
                 <div className="map">
-                    <Link to="/maps"><p>MAP HERE</p></Link>
+                    <p>MAP HERE</p>
                 </div>
                 <div className="map2">
                     <p>Map here</p>
